a35a33e46c4268cda7af95297ca51252cf9a132b
水剑 <123433403@qq.com>
2018-11-11 11:46:44 +0800

    init

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
-FROM registry.cn-shenzhen.aliyuncs.com/sjroom/alpine-java8-nginx
+FROM registry.cn-shenzhen.aliyuncs.com/sjroom/alpine-java8
 
-ADD service.sh /export/App/disconf.sjroom.cn/service.sh
+ADD service.sh /export/App/sjroom-admin/service.sh
 
-RUN mkdir -p /run/nginx/ && \
-    chmod +x /export/App/disconf.sjroom.cn/service.sh && \
-    echo "/export/App/disconf.sjroom.cn/service.sh start"  >> /export/servers/start.sh && \
-    echo "nginx"  >> /export/servers/start.sh && \
-    echo "/export/App/disconf.sjroom.cn/service.sh stop"  >> /export/servers/stop.sh
-
-ADD disconf-web/src/main/webapp/html /export/App/disconf.sjroom.cn/html
-COPY disconf-web/target/disconf-web.jar /export/App/disconf.sjroom.cn
+RUN  echo "/export/App/sjroom-admin/service.sh start"  >> /export/servers/start.sh
+RUN echo "/export/App/sjroom-admin/service.sh stop"  >> /export/servers/stop.sh
+COPY sjroom-admin-web/target/sjroom-admin-web.jar /export/App/sjroom-admin
---
+++ service.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+JAR_PATH="/export/App/sjroom-admin"
+JAR_NAME="sjroom-admin-web"
+
+
+function service_start(){
+    /export/servers/jdk/bin/java  -Denv=$env\
+    $JAVA_OPTS -jar "$JAR_PATH/$JAR_NAME.jar" > /dev/null 2>./error.log &
+}
+
+#function service_start(){
+#    /export/servers/jdk/bin/java -Dserver.port=8001 -Denv=$env\
+#     -Xms512M -Xmx1024M -XX:MaxDirectMemorySize=256M -XX:MetaspaceSize=128M -XX:MaxMetaspaceSize=256M \
+#     -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap \
+#     $JAVA_OPTS -jar "$JAR_PATH/$JAR_NAME.jar" > /dev/null 2>./error.log &
+#}
+
+function service_stop(){
+	kill -9 `/export/servers/jdk/bin/jps |grep $JAR_NAME|awk '{print $1}'` > /dev/null 2>&1 &
+}
+
+case $1 in
+        "start")
+                /export/servers/jdk/bin/jps |grep $JAR_NAME > /dev/null
+                if [ $? == 0 ];then
+                    echo "$JAR_NAME is already Started"
+                fi
+                echo "service_start"
+                service_start
+		sleep 1
+                /export/servers/jdk/bin/jps |grep $JAR_NAME > /dev/null
+                if [ $? == 0 ];then
+                    echo "$JAR_NAME is Started!"
+                    exit 0
+                else
+                    echo "$JAR_NAME start Fail!"
+                    exit 1
+                fi
+                ;;
+        "stop")
+                service_stop
+                sleep 1
+                /export/servers/jdk/bin/jps |grep $JAR_NAME > /dev/null
+                if [ $? == 0 ];then
+                        echo "$JAR_NAME stop Fail!"
+                        exit 1
+                else
+                        echo "$JAR_NAME is Stoped!"
+                        exit 0
+                fi
+                ;;
+        "restart")
+                service_stop
+                sleep 1
+                /export/servers/jdk/bin/jps |grep $JAR_NAME > /dev/null
+                if [ $? == 0 ];then
+                        echo "$JAR_NAME stop Fail!"
+                        exit 1
+                else
+                        echo "$JAR_NAME is Stoped!"
+                fi
+                service_start
+                /export/servers/jdk/bin/jps |grep $JAR_NAME > /dev/null
+                if [ $? == 0 ];then
+                    echo "$JAR_NAME is Started!"
+                    exit 0
+                else
+                    echo "$JAR_NAME start Fail!"
+                    exit 1
+                fi
+                ;;
+esac
